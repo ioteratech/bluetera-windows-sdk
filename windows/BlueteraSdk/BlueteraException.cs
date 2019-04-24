d19c3fc1ec50988c91a5cd876946df2b9b92b352
Boaz Aizenshtark <boaz.aizenshtark@gmail.com>
2019-04-24 20:21:28 +0300

    added missing sources

---
+++ windows/BlueteraSdk/BlueteraException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bluetera
+{
+    public class BlueteraException : Exception
+    {
+        public BlueteraException()
+        {
+        }
+
+        public BlueteraException(string message)
+            : base(message)
+        {
+        }
+
+        public BlueteraException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
