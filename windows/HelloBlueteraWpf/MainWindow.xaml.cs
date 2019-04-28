3648a5856ee121723115c008e177bf97aa9cace7
Boaz Aizenshtark <boaz.aizenshtark@gmail.com>
2019-04-28 13:35:42 +0300

    some changes to layout

--- windows/HelloBlueteraWpf/MainWindow.xaml.cs
+++ windows/HelloBlueteraWpf/MainWindow.xaml.cs
@@ -21,18 +21,18 @@ using Windows.Devices.Bluetooth.GenericAttributeProfile;
 
 namespace HelloBlueteraWpf
 {
-    public enum ApplicationStatusType { Idle, Scanning, Connecting, Connected, Disconnecting, Failed }
-
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         #region Constants / Enums
-        
-        #endregion        
+        public enum ApplicationStateType { Idle, Scanning, Connecting, Connected, Disconnecting, Error }
+        #endregion
 
         #region Lifecycle
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
         }
 
         protected override void OnSourceInitialized(EventArgs e)
@@ -42,9 +42,9 @@ namespace HelloBlueteraWpf
             _sdk = BlueteraSdk.Instance;
             _sdk.AdvertismentReceived += _sdk_AdvertismentReceived;
 
-            Status = ApplicationStatusType.Idle;
+            ApplicationState = ApplicationStateType.Idle;
         }
-        #endregion        
+        #endregion
 
         #region UI
         #region INotifyPropertyChanged
@@ -53,33 +53,35 @@ namespace HelloBlueteraWpf
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion        
+
+        #region Bound Properties
+        private ApplicationStateType _applicationState = ApplicationStateType.Idle;
+        public ApplicationStateType ApplicationState
+        {
+            get { return _applicationState; }
+            set { _applicationState = value; NotifyPropertyChanged(); }
+        }
         #endregion
 
         #region Event handlers
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
+            ApplicationState = ApplicationStateType.Scanning;
             _sdk.StartScan();
         }
 
         private void ResetCubeButton_Click(object sender, RoutedEventArgs e)
         {
-
+            _q0 = _qt;
         }
-
-        #region Bound Properties
-        private ApplicationStatusType _status = ApplicationStatusType.Idle;
-        public ApplicationStatusType Status
-        {
-            get { return _status; }
-            set { _status = value; NotifyPropertyChanged(); }
-        }
-        #endregion
         #endregion
         #endregion
 
         #region Bluetera
         #region Fields
         private Quaternion _q0 = Quaternion.Identity;
+        private Quaternion _qt = Quaternion.Identity;
         private BlueteraSdk _sdk;
         private BlueteraDevice _bluetera;
         #endregion
@@ -91,24 +93,34 @@ namespace HelloBlueteraWpf
             {
                 try
                 {
-                    Status = ApplicationStatusType.Connecting;
+                    _sdk.StopScan();
+
+                    ApplicationState = ApplicationStateType.Connecting;
 
                     // Try connecting to Bluetera                    
                     _bluetera = await _sdk.Connect(args.BluetoothAddress);  // This method will either connect or throw
                     _bluetera.ConnectionStatusChanged += _bluetera_ConnectionStatusChanged;
                     _bluetera.DownlinkMessageReceived += _bluetera_DownlinkMessageReceived;
-                    Status = ApplicationStatusType.Connected;
+
+                    ApplicationState = ApplicationStateType.Connected;
+                    DeviceLabel.Content = _bluetera.AddressAsString;
 
                     // Configure and Start IMU. Methods will throw on failure
                     await ConfigureImu();
                     await StartImu();
                 }
-                catch(BlueteraException)
+                catch (BlueteraException)
                 {
-                    Status = ApplicationStatusType.Failed;
+                    if (_bluetera != null)
+                    {
+                        _bluetera.Dispose();
+                        _bluetera = null;
+                    }
+
+                    ApplicationState = ApplicationStateType.Error;
                 }
             });
-        }        
+        }
 
         private void _bluetera_ConnectionStatusChanged(BlueteraDevice sender, BluetoothConnectionStatus args)
         {
@@ -122,21 +134,23 @@ namespace HelloBlueteraWpf
 
                     // re-enable IMU
                     if (args == BluetoothConnectionStatus.Connected)
-                    {                        
+                    {
                         await ConfigureImu();
                         await StartImu();
-                        Status = ApplicationStatusType.Connected;
+                        ApplicationState = ApplicationStateType.Connected;
+                        DeviceLabel.Content = _bluetera.AddressAsString;
                     }
                     else
                     {
                         _bluetera.Dispose();
                         _bluetera = null;
-                        Status = ApplicationStatusType.Idle;
+                        ApplicationState = ApplicationStateType.Idle;
+                        DeviceLabel.Content = null;
                     }
                 }
-                catch(BlueteraException)
+                catch (BlueteraException)
                 {
-                    Status = ApplicationStatusType.Failed;
+                    ApplicationState = ApplicationStateType.Error;
                 }
             });
         }
@@ -145,13 +159,13 @@ namespace HelloBlueteraWpf
         {
             Dispatcher.InvokeAsync(() =>
             {
-                switch(args.PayloadCase)
+                switch (args.PayloadCase)
                 {
                     case DownlinkMessage.PayloadOneofCase.Quaternion:
                         {
                             // apply rotation
-                            Quaternion q = new Quaternion(args.Quaternion.X, args.Quaternion.Y, args.Quaternion.Z, args.Quaternion.W);
-                            CubeModel.Transform = new RotateTransform3D(new QuaternionRotation3D(q));
+                            _qt = new Quaternion(args.Quaternion.X, args.Quaternion.Y, args.Quaternion.Z, args.Quaternion.W);
+                            CubeModel.Transform = new RotateTransform3D(new QuaternionRotation3D(_qt));
                         }
                         break;
 
@@ -164,7 +178,6 @@ namespace HelloBlueteraWpf
         #endregion
 
         #region Helpers
-        #endregion
         private async Task ConfigureImu()
         {
             UplinkMessage msg = new UplinkMessage()
@@ -186,7 +199,7 @@ namespace HelloBlueteraWpf
 
         private async Task StartImu()
         {
-            UplinkMessage msg = new UplinkMessage() 
+            UplinkMessage msg = new UplinkMessage()
             {
                 Imu = new ImuCommand()
                 {
@@ -216,6 +229,7 @@ namespace HelloBlueteraWpf
             if (status != GattCommunicationStatus.Success)
                 throw new BlueteraException($"Operation failed. Status = {GattCommunicationStatus.Success}");
         }
+        #endregion
         #endregion
     }
 }
