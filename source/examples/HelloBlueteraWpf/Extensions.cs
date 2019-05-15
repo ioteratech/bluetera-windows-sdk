b924b4cfb5d62aefed72efd7b65d1e7094dadedf
Boaz Aizenshtark <boaz.aizenshtark@gmail.com>
2019-05-15 17:32:01 +0300

    rearranged project

---
+++ source/examples/HelloBlueteraWpf/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace HelloBlueteraWpf
+{
+    public static class QuaternionExtensions
+    {
+        /// <summary>
+        /// Returns Euler Angles of the given Quaternion.
+        /// </summary>
+        /// <param name="q">The given Quaternion</param>
+        /// <param name="isDegrees">Return angle in Degrees (default) or Radians</param>
+        /// <returns>double[] - [phi (roll), theta (pitch), psi (yaw)]</returns>
+        /// <see cref="https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles"/>
+        public static double[] GetEuelerAngles(this Quaternion q, bool isDegrees = true)
+        {
+            double factor = isDegrees ? (180.0 / Math.PI) : 1.0;
+
+            double phi = Math.Atan2(2.0 * (q.W * q.X + q.Y * q.Z), 1.0 - 2.0 * (q.X * q.X + q.Y * q.Y));
+            double theta = Math.Asin(2.0 * (q.W * q.Y - q.Z * q.X));
+            double psi = Math.Atan2(2.0 * (q.W * q.Z + q.X * q.Y), 1.0 - 2.0 * (q.Y * q.Y + q.Z * q.Z));
+
+            return new double[] { factor * phi, factor * theta, factor * psi };
+        }
+
+
+    };
+}
--- source/gen-messages.bat
+++ source/gen-messages.bat
@@ -3,6 +3,6 @@ echo off
 where protoc
 if %ERRORLEVEL% NEQ 0 echo protocol buffers compiler was not found
 
-for %%p in (HelloBlueteraWinRt HelloBlueteraWpf) DO (
+for %%p in ("examples/HelloBlueteraWinRt" "examples/HelloBlueteraWpf") DO (
     protoc -I=../protobuf --csharp_out=%%p ../protobuf/bluetera_messages.proto
 )
